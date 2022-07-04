69b4c95c50cea64976e1d28139051b4453bea1ac
mesfen <mesfen145mogess@gmail.com>
2022-07-04 21:27:16 +0300

    no c

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
-    a = np.array([[1,2,3],[3,4,5],[7,8,9]])
-
-    s = [[str(e) for e in row] for row in a]
-    lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-    table = [fmt.format(*row) for row in s]
-    print ('\n'.join(table))
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
