f779ae7713bf4e393189f619bf8844573fe79a0c
mesfen <mesfen145mogess@gmail.com>
2022-07-04 20:41:21 +0300

    no

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-
 def no_c(my_string):
-    word = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            word = word + my_string[i]
-            return (word)
+    output = my_string.translate({ord(i): None fori in 'cC'})
+    return output
