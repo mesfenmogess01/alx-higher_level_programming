5854a2e7453b7b53eb10cae959faf35f196285b6
mesfen <mesfen145mogess@gmail.com>
2022-07-04 20:11:45 +0300

    no

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 
 
 def no_c(my_string):
-        word = ""
-            for i in range(len(my_string)):
-                        if (my_string[i] != 'c' and my_string[i] != 'C'):
-                                        word = word + my_string[i]
-                                            return (word)
+    word = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            word = word + my_string[i]
+            return (word)
