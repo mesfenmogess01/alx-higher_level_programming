6e5eabbe703e66d109fbc895a7d0003d52d79c2b
mesfen <mesfen145mogess@gmail.com>
2022-07-04 21:51:28 +0300

    delete

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    """divisible_by_2 - disvisibel by two"""
     boolist = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
