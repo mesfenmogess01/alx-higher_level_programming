19aed3d6e13506ecd86f57c935f45711df056bac
mesfen <mesfen145mogess@gmail.com>
2022-07-04 21:09:59 +0300

    print matrix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
-""" Returns an element in a liust"""
-if idx < 0 or idx > len(my_list) - 1:
- return None
-else:
- return my_list[idx]
+        return(my_list[idx] if 0 <= idx < len(my_list) else "None")
