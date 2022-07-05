7c8e4144357911f3ec6082048425a4d1b01ccdaa
mesfen <mesfen145mogess@gmail.com>
2022-07-05 18:27:08 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-from sys import argv
-
-num = len(argv)
-if num - 1 == 0:
-    print("0 arguments.")
-elif num - 1 == 1:
-    print("{:d} argument:".format(num - 1))
-else:
-    print("{:d} arguments:".format(num - 1))
-for i in range(1, num):
-    print("{:d}: {}".format(i, argv[i]))
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
