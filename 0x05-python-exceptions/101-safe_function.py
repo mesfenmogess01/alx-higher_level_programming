0a1a9ad0fbad204fb70d4145ed3d6b94f745c0c6
mesfen <mesfen145mogess@gmail.com>
2022-07-19 00:32:08 +0300

    advanced task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+# 101-safe_function.py
+
+
 import sys
 
 
 def safe_function(fct, *args):
+    """execute func
+    Args:
+    fct: func to exec.
+    args: fcr args.
+    Returns:
+    If error  -> None.
+    Else -> the output of the call to fct.
+    """
     try:
-        res = fct(*args)
-        return res
-    except Exception as err:
-        sys.stdout.write("Exception: " + str(err) + "\n")
-        return
+        output = fct(*args)
+        return (output)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
