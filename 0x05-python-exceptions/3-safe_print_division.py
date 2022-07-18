b16f5e38d2caf9d10c71e4ada42bee3cece34f02
mesfen <mesfen145mogess@gmail.com>
2022-07-18 23:58:18 +0300

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,11 +2,11 @@
 
 
 def safe_print_division(a, b):
-quotient = None
-try:
-    quotient = a / b
-    print("Inside result: {}".format(quotient))
-except:
-    print("Inside result: {}".format(quotient))
-finally:
-    return quotient
+    quotient = None
+    try:
+        quotient = a / b
+        print("Inside result: {}".format(quotient))
+    except:
+        print("Inside result: {}".format(quotient))
+    finally:
+        return quotient
