e18203e2994a2d2287460f216f324ca24991266f
mesfen <mesfen145mogess@gmail.com>
2022-07-18 19:48:36 +0300

    print integere

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
-    """ ptthon printing integer"""
     try:
         print("{:d}".format(value))
+        return True
     except:
         return False
-    return True
