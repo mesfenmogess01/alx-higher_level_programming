e1182a72ceaf29278e17a47b7e83944c7331c22b
mesfen <mesfen145mogess@gmail.com>
2022-07-19 23:11:23 +0300

    aquare

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+M0dule 2-square
+Define class Square
+"""
 
 
 class Square:
-    """Represent a square."""
+    """ Defines a Class square object.
+    Private instance attribute: size.
+    """
 
     def __init__(self, size=0):
-    """Initialize a new Square.
-
-    Args:
-    size (int): The size of the new square.
+    """ initialize the method square object
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    self.__size = size
+    else:
+        self.__size = int(size)
