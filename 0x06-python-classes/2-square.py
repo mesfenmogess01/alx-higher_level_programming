17fa81d4df9cb9ade47c4b1f9311d27135dc0786
mesfen <mesfen145mogess@gmail.com>
2022-07-19 23:29:02 +0300

    aquare

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,7 @@ class Square:
     """
 
     def __init__(self, size=0):
-    """ initialize the method square object
-    """
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
@@ -21,6 +20,4 @@ class Square:
         self.__size = int(size)
 
 def area(self):
-    """ Method that returns to the current the square area of the object
-    """
     return (self.__size ** 2)
