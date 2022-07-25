3477a559fcd3172f74caf37a1911f06f9b9ffc6b
mesfen <mesfen145mogess@gmail.com>
2022-07-25 17:37:48 +0300

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,14 +6,8 @@
      """Represent a rectangle."""
 
      def __init__(self, width=0, height=0):
-       """Initialize a new Rectangle.
-          Args:
-
-          width (int): The width of the new rectangle.
-          height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
+         self.width = width
+         self.height = height
 
     @property
     def width(self):
