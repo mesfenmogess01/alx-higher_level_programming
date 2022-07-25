801da16c4535e2c13726bf353f5fd5c061c7f112
mesfen <mesfen145mogess@gmail.com>
2022-07-25 17:52:47 +0300

    rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,19 +6,11 @@ This is a Rectangle class.
 
 
 class Rectangle:
-    '''class: Rectangle this is an empty class
-    '''
-
     def __init__(self, width=0, height=0):
-        '''method: __init__
-        initialize instance of Rectangle
-        '''
         self.__width = width
         self.__height = height
     @property
     def width(self):
-        '''method: set_width getter
-        '''
         if (not isinstance(self.__width, int)) or isinstance(self.__width,bool):
             raise TypeError("width must be an integer")
         if self.__width < 0:
@@ -27,8 +19,6 @@ class Rectangle:
 
     @width.setter
     def width(self, width):
-        '''method: set_width setter
-        '''
         if not isinstance(self.__width, int) or isinstance(self.__width, bool):
             raise TypeError("width must be an integer")
         if self.__width < 0:
@@ -37,8 +27,6 @@ class Rectangle:
 
     @property
     def height(self):
-        '''method: set_height getter
-        '''
         if (not isinstance(self.__height, int)) or isinstance(self.__height, bool):
             raise TypeError("height must be an integer")
         if self.__height < 0:
@@ -47,24 +35,15 @@ class Rectangle:
     
     @height.setter
     def height(self, height):
-        '''method: set_height
-        setter
-        '''
         if not isinstance(self.__height, int) or isinstance(self.__height, bool):
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-        
-        """
-        Calculate area of Rectangle.
-        """
+    
         def area(self):
             return self.__height * self.__width
 
-        """
-        Calculate perimeter of Rectangle object.
-        """
         def perimeter(self):
             if self.__height == 0 or self.width == 0:
                 return 0
