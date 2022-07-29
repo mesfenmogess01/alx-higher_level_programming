bec6b49434d4e3e9dad396d55f60e58eda1483eb
mesfen <mesfen145mogess@gmail.com>
2022-07-29 12:33:39 +0300

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-"""Defines func that adds two number
+
+"""
+   0-add_integer: add_integer()
 """
 
 
-def add_integer(a, b=98):
-    """Adds two nums
+def add_integer: add_integer(a, b=98):
     """
-    if a != int(a) and a != float(a):
-        raise TypeError("a must be int")
-    if b != int(b) and b != int(b):
-        raise TypeError("b must be int")
+    add_integer returns the sum of two integers
+    Args:
+        a: first number.
+        b: second number.
+    Returns:
+         sum of the two integers
+    """
+    if type(a) not in [int, float] or a is None:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float] or b is None:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     return a + b
