fdc1e177c788fe90d52a7070520fa9eafad06935
mesfen <mesfen145mogess@gmail.com>
2022-08-02 18:50:16 +0300

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,13 +8,13 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-       """method __init__
-       """
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
+        """method __init__
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-       """method to_json
-       """
-       return self.__dict__
+        """method to_json
+        """
+        return self.__dict__
