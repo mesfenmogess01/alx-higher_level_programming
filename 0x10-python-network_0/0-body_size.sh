c75d78f9b6b20d0ee5aac42a3bd09e77144d696d
mesfen <mesfen145mogess@gmail.com>
2022-10-20 23:30:47 +0300

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL request that displays size of the response's body
-curl -so /dev/null "$1" -w '%{size_download}\n'
+# a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
